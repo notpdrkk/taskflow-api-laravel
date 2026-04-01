341f1b530f4e22795227d8002a212e41ed22a981
notpdrkk <pedrohsrgpp@gmail.com>
2026-04-01 11:27:57 -0300

    feat: add project dto

--- app/Services/ProjectService.php
+++ app/Services/ProjectService.php
@@ -2,27 +2,37 @@
 
 namespace App\Services;
 
+use App\Exceptions\InvalidJsonFormat;
 use App\Models\Project;
 use Illuminate\Http\Request;
+use App\Models\DTO\ProjectResponseDTO;
 
 class ProjectService
 {
 
     public function index()
     {
-        return Project::all();
+        try {
+            return ProjectResponseDTO::all()->from(Project::all());
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'An unexpected error occurred'], 500);
+        }
     }
-
     public function store(Request $request)
     {
-        $validated = $request->validate([
-            'name' => 'required|string|max:100',
-            'description' => 'nullable|max:255',
-            'status' => 'required',
-            'deadline' => 'nullable|date'
-        ]);
-
-        $project = Project::create($validated);
-        return $project;
+        try {
+            $validated = $request->validate([
+                'name' => 'required|string|max:100',
+                'description' => 'nullable|max:255',
+                'status' => 'required',
+                'deadline' => 'nullable|date'
+            ]);
+            $project = Project::create($validated);
+            return ProjectResponseDTO::from($project);
+        } catch (InvalidJsonFormat $e) {
+            return response()->json(['invalid json format' => $e->getMessage()], 400);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 }
--- database/migrations/2026_03_26_133826_create_projects_table.php
+++ database/migrations/2026_03_26_133826_create_projects_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->default(1)->constrained('users');
+            $table->foreignId('user_id')->constrained('users');
             $table->string('name');
             $table->string('description')->nullable();
             $table->enum('status', ['pending', 'in_progress', 'done']);
